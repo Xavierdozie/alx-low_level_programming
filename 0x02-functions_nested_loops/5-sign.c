da5aa046d2e76a713a1cf4e447964bae5d9e4826
Chidozie Ugwuanyi <chidozieugwuanyi82@gmail.com>
2022-04-12 18:03:56 -0700

    functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  * print_sign - Determine if the input number
  * greater, equal or less than zero
  *
- * @c: The input as an integer.
+ * @n: The input number as an integer.
  *
  * Return: 1 is greater then zero. 0 is zero
  * -1 is less than zero.
