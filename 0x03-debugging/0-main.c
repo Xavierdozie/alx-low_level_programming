680d302c9752eb647111f0d72799366730242bc8
Chidozie Ugwuanyi <chidozieugwuanyi82@gmail.com>
2022-04-15 03:51:40 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Returns: 0
  */
 
