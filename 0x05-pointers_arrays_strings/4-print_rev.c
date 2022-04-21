4f4e49f89257427dade1cfd443e152e82f3e9576
Chidozie Ugwuanyi <chidozieugwuanyi82@gmail.com>
2022-04-21 14:27:19 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
  * print_rev - prints a string reversed
- *
  * @s: pointer
  */
 void print_rev(char *s)
 {
 int i = 0;
 
-while (s[i])
+ while (s[i])
  i++;
-while (i--)
-_putchar(s[i]);
-__putchar('\n');
+ while (i--)
+ _putchar(s[i]);
+_putchar('\n');
 }
