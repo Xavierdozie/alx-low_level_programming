beb3b4fc885723e47373a8ac36617b2544220eae
Chidozie Ugwuanyi <chidozieugwuanyi82@gmail.com>
2022-04-20 19:29:47 -0700

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,10 +9,10 @@ void rev_string(char *s)
 	int len, i, half;
 	char temp;
 	for (len = 0; s[len] != '\0'; len++)
-		;
+	;
 	i = 0;
 	half = len / 2;
-	while (half --)
+	while (half--)
 	{
 		temp = s[len - i - 1];
 		s[len - i - 1] = s[i];
