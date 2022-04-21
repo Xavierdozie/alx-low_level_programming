94aa4c3281c21a0654f1dd253ce9bce986d7ff71
Chidozie Ugwuanyi <chidozieugwuanyi82@gmail.com>
2022-04-21 13:53:01 -0700

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,23 +8,23 @@
 
 void puts_half(char *str)
 {
-	int len, mid;
-	for (len = 0; str[len] != '\0'; len++)
-		;
+int len, mid;
+for (len = 0; str[len] != '\0'; len++)
+;
 
-	if (len % 2 == 0)
-	{
-		mid = len / 2;
-	}
-	else
-	{
-		mid = ((len - 1) / 2) + 1;
-	}
+if (len % 2 == 0)
+{
+mid = len / 2;
+}
+else
+{
+mid = ((len - 1) / 2) + 1;
+}
 
-	while (mid < len)
-	{
-		_putchar(str[mid]);
-		mid++;
-	}
-	_putchar('\n');
+while (mid < len)
+{
+_putchar(str[mid]);
+mid++;
+}
+_putchar('\n');
 }
