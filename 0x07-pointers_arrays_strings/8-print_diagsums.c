3fd319c0ff3b7ed1f4b1a2737bfe5e7721e135f5
Chidozie Ugwuanyi <chidozieugwuanyi82@gmail.com>
2022-04-26 19:44:57 -0700

    more pointers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_chessboard - print the chessboard, 2d array
- * @a: 2d array of chars
- * Return: void
- */
-void print_chessboard(char (*a)[8])
-{
-	/*Declaring variables*/
-	int i, j;
+ * print_diagsums - print sum of diagonal values in an array
+ * @a: arrray of values
+ * @size: size of array
+ * Description: prints sum of both diagonals
+ * Return: nothing
+ **/
 
-	i = 0;
-	while (i < 8) /*number repetitions*/
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, x;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (x = 0; x < size; x++)
 	{
-		j = 0;
-		while (j < 8)
-		{
-			_putchar(a[i][j]);
-			j++;
-		}
-		_putchar('\n'); /*print new line*/
-		i++; /*add +1*/
+		sum1 = sum1 + a[x * size + x];
 	}
 
+	for (x = size - 1; x >= 0; x--)
+	{
+		sum2 += a[x * size + (size - x - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
