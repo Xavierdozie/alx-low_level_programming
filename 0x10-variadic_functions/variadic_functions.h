a4284fccfa0b26cb56b8540c854379b6150bc422
Chidozie Ugwuanyi <chidozieugwuanyi82@gmail.com>
2022-05-16 21:20:18 -0700

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,10 +2,10 @@
 #define variadic_functions_h
 #include <stddef.h>
 
- int _putchar(char c);
- int sum_them_all(const unsigned int n, ...);
- void print_numbers(const char *seperator, const unsigned int n, ...);
- void print_strings(const char *seperator, const unsigned int n, ...);
- void print_all(const char * const format, ...);
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
